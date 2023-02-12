28e6f88a523b7b71d585cb7bbf1ebb309e1e0280
ethanmidgley <ethan.c.midgley@gmail.com>
2023-02-12 19:02:05 +0000

    Fixed loading of config file

--- cmd/main.go
+++ cmd/main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/ethanmidgley/quick-folders/pkg/config"
 )
 
 func main() {
-
-	conf, err := config.Load("./config.yaml")
+	exPath, _ := os.Executable()
+	conf, err := config.Load(filepath.Join(exPath, "./config.yaml"))
 	if err != nil {
 		log.Panic(err)
 	}
